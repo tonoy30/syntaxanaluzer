96df562829ff2d73f047778fa82484de93f87236
Md. Tonoy Akando <tonoy.sust@gmail.com>
2022-11-26 18:05:54 +0600

    using collector

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using StntaxAnalyzer.SyntaxWalker;
 using static System.Console;
 
 const string programText =
@@ -23,47 +22,10 @@ const string programText =
 var tree = CSharpSyntaxTree.ParseText(programText);
 var root = tree.GetCompilationUnitRoot();
 
-WriteLine($"The tree is a {root.Kind()} node.");
-WriteLine($"The tree has {root.Members.Count} elements in it.");
-WriteLine($"The tree has {root.Usings.Count} using statements. They are:");
-foreach (var element in root.Usings)
-    WriteLine($"\t{element.Name}");
+var collector = new UsingCollector();
+collector.Visit(root);
 
-var firstMember = root.Members[0];
-WriteLine($"The first member is a {firstMember.Kind()}.");
-var helloWorldDeclaration = (NamespaceDeclarationSyntax) firstMember;
-// </Snippet4>
-
-// <Snippet5>
-WriteLine($"There are {helloWorldDeclaration.Members.Count} members declared in this namespace.");
-WriteLine($"The first member is a {helloWorldDeclaration.Members[0].Kind()}.");
-// </Snippet5>
-
-// <Snippet6>
-var programDeclaration = (ClassDeclarationSyntax) helloWorldDeclaration.Members[0];
-WriteLine(
-    $"There are {programDeclaration.Members.Count} members declared in the {programDeclaration.Identifier} class.");
-WriteLine($"The first member is a {programDeclaration.Members[0].Kind()}.");
-var mainDeclaration = (MethodDeclarationSyntax) programDeclaration.Members[0];
-// </Snippet6>
-
-// <Snippet7>
-WriteLine($"The return type of the {mainDeclaration.Identifier} method is {mainDeclaration.ReturnType}.");
-WriteLine($"The method has {mainDeclaration.ParameterList.Parameters.Count} parameters.");
-foreach (var item in mainDeclaration.ParameterList.Parameters)
-    WriteLine($"The type of the {item.Identifier} parameter is {item.Type}.");
-WriteLine($"The body text of the {mainDeclaration.Identifier} method follows:");
-WriteLine(mainDeclaration.Body.ToFullString());
-
-var argsParameter = mainDeclaration.ParameterList.Parameters[0];
-// </Snippet7>
-
-// <Snippet8>
-var firstParameters = from methodDeclaration in root.DescendantNodes()
-        .OfType<MethodDeclarationSyntax>()
-    where methodDeclaration.Identifier.ValueText == "Main"
-    select methodDeclaration.ParameterList.Parameters.First();
-
-var argsParameter2 = firstParameters.Single();
-
-WriteLine(argsParameter == argsParameter2);
+foreach (var directive in collector.Usings)
+{
+    WriteLine(directive.Name);
+}
--- SyntaxWalker/UsingCollector.cs
+++ SyntaxWalker/UsingCollector.cs
@@ -1,6 +1,19 @@
-namespace SyntaxAnalyzer.SyntaxWalker;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static System.Console;
 
-public class UsingCollector
+namespace StntaxAnalyzer.SyntaxWalker;
+
+public class UsingCollector : CSharpSyntaxWalker
 {
-    
+    public ICollection<UsingDirectiveSyntax> Usings { get; } = new List<UsingDirectiveSyntax>();
+
+    public override void VisitUsingDirective(UsingDirectiveSyntax node)
+    {
+        WriteLine($"\tVisit Using Directive called with {node.Name}.");
+        if (node.Name.ToString() == "System" ||
+            node.Name.ToString().StartsWith("System.")) return;
+        WriteLine($"\t\tSuccess. Adding {node.Name}."); 
+        Usings.Add(node);
+    }
 }
